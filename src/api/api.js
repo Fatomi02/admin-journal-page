6fec116da85fd0afac45ea7ff0e465cbd81aebde
Fatomi02 <fatomiabdulrahmon@gmail.com>
2025-01-20 22:38:26 +0000

    done

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://api-aco-pro.onrender.com'
+    baseURL: 'https://api-aco-boss.onrender.com'
 });
 
 api.interceptors.request.use((config) => {
