eacf95037bece90628b267aac532dc99028a99af
Fatomi02 <fatomiabdulrahmon@gmail.com>
2025-01-20 14:42:54 +0000

    done

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://api-aco-page.onrender.com'
+    baseURL: 'https://api-aco-pro.onrender.com'
 });
 
 api.interceptors.request.use((config) => {
